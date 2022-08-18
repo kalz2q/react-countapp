8f1a3f26196a7dcd0b91a6525b6609fe9f361157
kalz2q <katsu95i@gmail.com>
2022-08-18 09:16:33 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const onDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{count}</div>
+      <div>
+        <button type="button" onClick={onIncrement}>
+          +
+        </button>
+        <button type="button" onClick={onDecrement}>
+          -
+        </button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
