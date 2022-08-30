4c570898237cd4fab7403c04103c56cd75ababf5
kalz2q <katsu95i@gmail.com>
2022-08-31 00:44:04 +0900

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 const App = () => {
+  useEffect(() => {
+    document.title = "react-countapp";
+  });
+
   const [count, setCount] = useState(0);
-
-  const onIncrement = () => {
-    setCount((prevCount) => prevCount + 1);
+  type Msg = "Increment" | "Decrement";
+  type BButtonProps = {
+    value: Msg;
+  };
+  const BButton = (props: BButtonProps) => {
+    switch (props.value) {
+      case "Increment":
+        setCount((prevCount) => prevCount + 1);
+        return <button onClick={onclick}>"+"</button>;
+        break;
+      case "Decrement":
+        setCount((prevCount) => prevCount - 1);
+        return  <button onClick={onclick}>"-"</button>;
+        break;
+      default: 
+        return <button onClick={onclick}>"*****"</button>;
+    }
   };
 
-  const onDecrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+
+  // const onIncrement = () => {
+  //   setCount((prevCount) => prevCount + 1);
+  // };
+
+  // const onDecrement = () => {
+  //   setCount((prevCount) => prevCount - 1);
+  // };
 
   return (
     <div>
       <div>{count}</div>
       <div>
-        <button type="button" onClick={onIncrement}>
+        {/* <button type="button" onClick={onIncrement}>
           +
         </button>
         <button type="button" onClick={onDecrement}>
           -
-        </button>
+        </button> */}
+        <BButton value="Increment" />
+        <BButton value="Decrement"  />
       </div>
     </div>
   );
 };
+
 export default App;
